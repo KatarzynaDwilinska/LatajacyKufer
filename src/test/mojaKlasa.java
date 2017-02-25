8da9ed7b1cb11de590eedf7fc2ad2d5888965019
Katarzyna Dwilińska <katarzyna.dwilinska@gmail.com>
2017-02-25 02:29:19 +0100

    kosmetyka

--- src/test/mojaKlasa.java
+++ src/test/mojaKlasa.java
@@ -2,6 +2,6 @@ package test;
 
 public class mojaKlasa {
 	  public static void main(String[] args) {
-		    System.out.println("Kocham mojego Daguszka!!!!");
+		    System.out.println("Kocham mojego Daguszka!!!! <3");
 		  }
 }
